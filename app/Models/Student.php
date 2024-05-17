14d07cc7d2ed0a4f1f518da146e6ed974a216fc1
Ali_2671994 <ar.mourtada1@gmail.com>
2024-05-17 20:35:09 +0300

    feat: add sanctum to the project

--- app/Models/Student.php
+++ app/Models/Student.php
@@ -1,11 +1,10 @@
 <?php
 
 namespace App\Models;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Laravel\Sanctum\HasApiTokens;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
-
-class Student extends Model
+class Student extends Authenticatable
 {
-    use HasFactory;
+    use HasApiTokens;
 }
