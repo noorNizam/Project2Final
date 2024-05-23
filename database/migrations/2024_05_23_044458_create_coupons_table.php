e6d565535e9e78c9b9c89e6c3ee21560c24a83b4
EnasAlhesan <132252041+EnasAlhesan@users.noreply.github.com>
2024-05-23 08:49:48 +0300

    feet: add coupon model and migration file

---
+++ database/migrations/2024_05_23_044458_create_coupons_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('coupons', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->unsignedBigInteger('course_id');
+            $table->foreign('course_id')->references('id')->on('courses');
+            $table->string('name');
+            $table->integer('count');
+            $table->integer('value');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('coupons');
+    }
+};
