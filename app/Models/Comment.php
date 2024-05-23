4887b6943d937447971a81238c64d7df96f4b423
EnasAlhesan <132252041+EnasAlhesan@users.noreply.github.com>
2024-05-23 08:49:23 +0300

    feet: add comment model and migration file

---
+++ app/Models/Comment.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Comment extends Model
+{
+    use HasFactory;
+    protected $fillable=['video_id','student_id','comment'];
+    public function video (){
+        return $this->belongsTo(Video::class);
+    }
+}
---
+++ database/migrations/2024_05_23_052610_create_comments_table.php
@@ -0,0 +1,39 @@
+
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('video_id');
+            $table->foreign('video_id')->references('id')->on('videos')->onDelete('cascade');
+            $table->unsignedBigInteger('student_id');
+            $table->foreign('student_id')->references('id')->on('students')->onDelete('cascade');
+            $table->string('comment');
+           
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comments');
+    }
+};
