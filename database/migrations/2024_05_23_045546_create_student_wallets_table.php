fd3e1c4ddc3e14ef4815ab3dd4db308ec9fbed23
EnasAlhesan <132252041+EnasAlhesan@users.noreply.github.com>
2024-05-23 08:46:58 +0300

    feet: add student_wallet model and migration file

---
+++ database/migrations/2024_05_23_045546_create_student_wallets_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('student_wallets', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('student_id');
+            $table->foreign('student_id')->references('id')->on('students');
+            $table->integer('value')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('student_wallets');
+    }
+};
